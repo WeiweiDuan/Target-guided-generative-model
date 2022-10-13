aa4b3281a407f9fc75e9f0d41806c521eacb9d8f
Weiwei Duan <weiweidu@usc.edu>
2022-10-12 18:14:01 -0700

    Update gen_train_data.py

--- gen_train_data.py
+++ gen_train_data.py
@@ -1,6 +1,7 @@
 from shutil import copyfile
 import os
 import argparse
+import numpy as np
 from utils.helper import create_folder, remove_files, load_file_names
 
 '''
@@ -10,16 +11,17 @@ if the computation resources are limited, please select a part of cropped images
 '''
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--dataset_name", type=str, default='COWC)
-parser.add_argument("--loc_idx", type=str, default='Toronto_03559.8.2')
+parser.add_argument("--dataset_name", type=str, default='COWC')
+parser.add_argument("--loc_idx", type=str, default='Toronto_03553.7.8')
 parser.add_argument("--obj_name", type=str, default='car')
-parser.add_argument("--pos_names", type=list, default=['100_100'])
+parser.add_argument("--pos_names", type=str, default='100_100',
+                   help="labeled image name, separated by comma")
 parser.add_argument("--sample_portion", type=float, default=1.0)
 args = parser.parse_args()
 
 unlabeled_imgs_path = './temp'
 # the name of chosen target images, named by top_left coordinate
-pos_names = args.pos_names
+pos_names = args.pos_names.split(',')
 img_type = '.png'
 sample_portion = args.sample_portion
 
@@ -29,15 +31,15 @@ obj_name = args.obj_name
 #location name or index in the dataset
 loc_idx = args.loc_idx
                     
-sample_path = os.path.join('data', dataset_name, obj_name)
-loc_name = '_'.join([loc_idx, obj_name])
+sample_path = os.path.join('./data', dataset_name)
+loc_name = loc_idx
 
-pos_folder_path = os.path.join(sample_path, loc_name+'_samples', 'positive')
-subset_folder_path = os.path.join(sample_path, loc_name+'_samples', 'subset')
+pos_folder_path = os.path.join(sample_path, loc_name, 'positive')
+subset_folder_path = os.path.join(sample_path, loc_name, 'subset')
 
 # create a folder to save the target images, if exist, clean it first
 create_folder(pos_folder_path)
-remove_files(pos_folder_path, 'png')
+remove_files(pos_folder_path)
 
 
 for img_name in pos_names:
@@ -51,7 +53,7 @@ print('num of cropped images: ', num_imgs)
 
 
 create_folder(subset_folder_path)
-remove_files(subset_folder_path, 'png')
+remove_files(subset_folder_path)
 
 img_names = load_file_names(unlabeled_imgs_path)
 img_names = np.array(img_names)
@@ -62,3 +64,6 @@ for i in range(num_samples):
     src = os.path.join(unlabeled_imgs_path, img_name+img_type)
     dst = os.path.join(subset_folder_path, img_name+img_type)
     copyfile(src, dst)
+
+print('==== the labeled positive images are saved in %s ===='%(pos_folder_path))
+print('==== all cropped images within the region are saved in %s ===='%(subset_folder_path))
